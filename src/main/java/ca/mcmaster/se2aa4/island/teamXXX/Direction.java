2219334aade665b9b5e5baee5956bfc85b0c6519
shamilemir <shamilcanbolat@gmail.com>
2025-03-12 13:46:38 -0400

    direction enum

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Direction.java
@@ -1,8 +1,37 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
 public enum Direction {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
+    NORTH(null, null),
+    EAST(null, null), 
+    SOUTH(null, null), 
+    WEST(null, null);
+
+    private Direction rightDirection;
+    private Direction leftDirection;
+
+    Direction (Direction rightDirection, Direction leftDirection) {
+
+    }
+
+    static {
+        NORTH.rightDirection = EAST;
+        NORTH.leftDirection = WEST;
+
+        EAST.rightDirection = SOUTH;
+        EAST.leftDirection = NORTH;
+
+        SOUTH.rightDirection = WEST;
+        SOUTH.leftDirection = EAST;
+
+        WEST.rightDirection = NORTH;
+        WEST.leftDirection = SOUTH;
+    }
+
+    public Direction getRightDirection() {
+        return rightDirection;
+    }
+    public Direction getLeftDirection() {
+        return leftDirection;
+    }
+
 }
