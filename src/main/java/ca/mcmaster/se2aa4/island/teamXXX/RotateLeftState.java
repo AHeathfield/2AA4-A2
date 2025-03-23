619a0f974b83c69615e52aa6ca815840258e72a0
Aidan Heathfield <aidan.heathfield@gmail.com>
2025-03-22 21:29:30 -0400

    Utility #10: Made RotateLeftState, rotates the drone to the left and move forward 1

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/RotateLeftState.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/RotateLeftState.java
@@ -7,14 +7,14 @@ import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 
 // This is too help traverse coast line, it does not consider edge cases
-public class RotateRightState extends State {
+public class RotateLeftState extends State {
     private final Logger logger = LogManager.getLogger();
     private List<Instruction> sequenceOfInstructs;
     private int instructCount;  // To keep track of what instruct we are on
     private Direction droneDir;
 
     // Constructor
-    public RotateRightState(RescueComputer computer) {
+    public RotateLeftState(RescueComputer computer) {
         super(computer);
         this.instructCount = 0;
         this.droneDir = computer.getDroneDirection();
@@ -26,15 +26,15 @@ public class RotateRightState extends State {
     private void setupInstructions() {
         this.sequenceOfInstructs = new ArrayList<>();
 
-        // Setting up sequence of instructs FLY, 3 HEAD.lefts, 2 FLY
+        // Setting up sequence of instructs FLY, 3 HEAD.rights, 2 FLY
         sequenceOfInstructs.add(new Instruction(Action.FLY));
 
         // Need to add this 3 times
         for (int i = 0; i < 3; i++) {
             JSONObject param = new JSONObject();
             logger.info("Current Drone Dir: {}", droneDir);
-            param.put("direction", droneDir.getLeftDirection().toString());
-            droneDir = droneDir.getLeftDirection();
+            param.put("direction", droneDir.getRightDirection().toString());
+            droneDir = droneDir.getRightDirection();
             sequenceOfInstructs.add(new Instruction(Action.HEADING, param));
         }
 
@@ -42,7 +42,7 @@ public class RotateRightState extends State {
         sequenceOfInstructs.add(new Instruction(Action.FLY));
     }
 
-    // Mimicking rotate player to right directions and move forward 1
+    // Mimicking rotate player to left directions and move forward 1
     @Override
     public Instruction determineNextInstruction(JSONObject droneResponse) {
         logger.info("Rotating right.... Current count = {}", instructCount);
