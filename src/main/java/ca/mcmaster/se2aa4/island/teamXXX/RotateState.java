475d3fe0e6f137d586fc421caece2082dfde7346
shamilemir <shamilcanbolat@gmail.com>
2025-03-23 19:10:40 -0400

    #18 kanban, refactor rotateleftstate and rotaterightstate into rotatestate

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/RotateState.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/RotateState.java
@@ -7,53 +7,68 @@ import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 
 // This is too help traverse coast line, it does not consider edge cases
-public class RotateBackwardsState extends State {
+public class RotateState extends State {
     private final Logger logger = LogManager.getLogger();
     private List<Instruction> sequenceOfInstructs;
     private int instructCount;  // To keep track of what instruct we are on
     private Direction droneDir;
 
     // Constructor
-    public RotateBackwardsState(RescueComputer computer) {
+    public RotateState(RescueComputer computer, Turn turn) {
         super(computer);
         this.instructCount = 0;
         this.droneDir = computer.getDroneDirection();
-        setupInstructions();
-
+        setupInstructions(turn);
     }
 
-    // Fills in list of instructs
-    private void setupInstructions() {
+    // Both fill in list of instructs
+    private void setupInstructions(Turn turn) {
         this.sequenceOfInstructs = new ArrayList<>();
 
-        // Setting up sequence of instructs FLY, 3 HEAD.rights, HEAD.left
+        // Setting up sequence of instructs 
+        // Left: FLY, 3 HEAD.rights, 2 FLY
+        // Right: FLY, 3 HEAD.lefts, 2 FLY
         sequenceOfInstructs.add(new Instruction(Action.FLY));
 
         // Need to add this 3 times
         for (int i = 0; i < 3; i++) {
             JSONObject param = new JSONObject();
             logger.info("Current Drone Dir: {}", droneDir);
-            param.put("direction", droneDir.getRightDirection().toString());
-            droneDir = droneDir.getRightDirection();
+
+            if (turn == Turn.LEFT) {
+                droneDir = droneDir.getLeftDirection(); 
+            } 
+            else if (turn == Turn.RIGHT) {
+                droneDir = droneDir.getRightDirection();
+            }
+
+            param.put("direction", droneDir.toString()); //already turned, so just toString()
+            
             sequenceOfInstructs.add(new Instruction(Action.HEADING, param));
         }
-        
-        JSONObject param = new JSONObject();
-        param.put("direction", droneDir.getLeftDirection().toString());
-        droneDir = droneDir.getLeftDirection();
-        sequenceOfInstructs.add(new Instruction(Action.HEADING, param));
+
+        sequenceOfInstructs.add(new Instruction(Action.FLY));
+        sequenceOfInstructs.add(new Instruction(Action.FLY));
     }
 
-    // Mimicking rotate player to right directions and move forward 1
+    // Mimicking rotate player to right or left directions and move forward 1
     @Override
     public Instruction determineNextInstruction(JSONObject droneResponse) {
-        logger.info("Rotating right.... Current count = {}", instructCount);
+        
+        String rotation;
+        if (computer.getFormerDroneDirection().getRightDirection() == computer.getDroneDirection()) {
+            rotation = "Right";
+        } else {
+            rotation = "Left";
+        }
+
+        logger.info("Rotating {}.... Current count = {}", rotation, instructCount);
         Instruction nextInstruct = sequenceOfInstructs.get(instructCount);
         instructCount++;
         
         // If last instruction
         if (instructCount == sequenceOfInstructs.size()) {
-            logger.info("Right rotation COMPLETE!, final direction {}", droneDir);
+            logger.info("{} rotation COMPLETE!, final direction {}", rotation, droneDir);
             computer.setDroneDirection(droneDir);
             computer.setCurrentState(new PatrolCoastState(computer));
         }
